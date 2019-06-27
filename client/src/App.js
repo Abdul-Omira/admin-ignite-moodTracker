8b901d049368bd5e8cf0881c91a7b4c3f70a7a7a
Abdul omira <abdul@repocoin.io>
2019-06-26 23:58:07 -0500

    Final ready for prodaction

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,39 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
+import Routes from './components/routing/Routes';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
 import './App.css';
 
-const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
-      <Route exact path='/' component={Landing} />
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
 
-      <section className='container'>
-        <switch>
-          <Route exact path='/login' component={Login} />
-        </switch>
-      </section>
-    </Fragment>
-  </Router>
-);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Landing = () => {
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
+
   return (
     <section className='landing'>
       <div className='landing-inner'>
-        <h1 className='x-large'>IGnite Mood Tracker Admin Panel</h1>
-        <p className='lead'>Manege the mood tracker app data</p>
+        <h1 className='x-large'>Developer Connector</h1>
+        <p className='lead'>
+          Create a developer profile/portfolio, share posts and get help from
+          other developers
+        </p>
         <div className='buttons'>
           <Link to='/login' className='btn btn-light'>
             Login
@@ -17,4 +26,12 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps)(Landing);
